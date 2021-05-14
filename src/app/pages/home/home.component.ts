018fd7e5c62a45a6b81c6485de7b564406a763c9
Dani <salcedochanza@gmail.com>
2021-05-14 21:37:41 +0200

    Add header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private convidat: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
